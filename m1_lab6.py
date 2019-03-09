6874b606241f291533a777e5763f0ba1f7f4c799
1998dener <48300954+1998dener@users.noreply.github.com>
2019-03-09 18:59:52 -0300

    Create m1_lab6.py

---
+++ m1_lab6.py
@@ -0,0 +1,60 @@
+'''
+classe: m1_lab6.py
+descricao: Numero total de votos por partido
+autor: Clodonil Honorio Trigo
+email: clodonil@nisled.org
+data: 04 de julho de 2018
+'''
+
+# importa a lib para obter as tabelas da Wikipedia
+from  lib.scrapy_table import Scrapy_Table
+
+
+# Variavel com o link da tabela
+url="https://pt.wikipedia.org/wiki/C%C3%A2mara_Municipal_de_S%C3%A3o_Paulo"
+
+# Inicia a class para obter a tabela
+site_connect = Scrapy_Table(url)
+
+# Pegando a tabela 5 (Vereadores em exercicio)
+tables = site_connect.get_tables(5)
+  
+# Dicionario para armazenar partido e votos
+# key = partidos
+# values = votos
+partidos =  {}
+
+'''for linha in tables[1:]:
+
+    # Obtendo o numo de votos do cantidato
+    print(linha[2])
+'''
+    
+print("___________________________________________________________________________________________")
+print()
+print("Segue a lista de partidos encontrados com seus respectivos votos:")
+print()
+
+# Listando o conteudo da tabela
+for linha in tables[1:]:
+  
+    # Obtendo o partido
+    partido = linha[1]
+    print(partido)
+    print(linha[2])
+    print()
+
+    # Obtendo o voto do partido
+    n_voto = float(linha[2].split(" ")[0])
+
+    # se o partido esta no dicionario
+    if partido in partidos:
+       # Se tiver soma os votos
+       partidos[partido] = partidos[partido] + n_voto
+    else:
+       # Se nao tiver inicializa o dicionario com o partido e o numero de votos
+       partidos[partido] = n_voto
+print()    
+# Imprime os partidos e os votos
+for partido in partidos:
+    print(partido, 'arrecadou',partidos[partido], 'votos de seus eleitores.') 
